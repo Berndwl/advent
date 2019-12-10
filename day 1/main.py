ec329edeee53e178cea8861bb5c0d01365885c40
Berndwl <Wolf@chat.org>
2019-12-10 17:56:09 +0100

    refactor day1

--- day 1/main.py
+++ day 1/main.py
@@ -1,10 +1,13 @@
-def calculate_fuel_recursed(fuel, fuel_sum=0):
+# Part 1
+print(sum((int(line.strip()) // 3) - 2 for line in open("input.txt", "r")))
+
+# Part 2
+def calculate_fuel_recursed(fuel):
     new_fuel = (fuel // 3) - 2
     if new_fuel > 0:
-        fuel_sum += new_fuel
-        return calculate_fuel_recursed(new_fuel, fuel_sum)
+        return new_fuel + calculate_fuel_recursed(new_fuel)
     else:
-        return fuel_sum
+        return 0
 
 
 print(sum(x for x in [calculate_fuel_recursed(int(line.strip())) for line in
