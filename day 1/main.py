a396592783b9ac68c48c84bb2d18716d84f0a80e
Berndwl <Wolf@chat.org>
2019-12-10 14:28:03 +0100

    pt2

--- day 1/main.py
+++ day 1/main.py
@@ -1,1 +1,12 @@
-total = sum((int(line.strip()) // 3) - 2 for line in open("input.txt", "r"))
+def calculate_fuel_recursed(fuel, fuel_sum=0):
+    new_fuel = (fuel // 3) - 2
+    if new_fuel > 0:
+        fuel_sum += new_fuel
+        return calculate_fuel_recursed(new_fuel, fuel_sum)
+    else:
+        return fuel_sum
+
+
+print(sum(x for x in [calculate_fuel_recursed(int(line.strip())) for line in
+                      open("input.txt", "r")]))
+
